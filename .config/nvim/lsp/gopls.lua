f770307a3779379d2613a5c61c3dffead0858948
Dominik Heller <kilianmandscharo@gmail.com>
2025-11-14 22:51:45 +0100

    add go language server

---
+++ .config/nvim/lsp/gopls.lua
@@ -0,0 +1,8 @@
+return {
+    cmd = {
+        "gopls",
+    },
+    filetypes = {
+        "go",
+    },
+}
--- .config/nvim/lua/core/lsp.lua
+++ .config/nvim/lua/core/lsp.lua
@@ -2,6 +2,7 @@ vim.lsp.enable({
     "lua_ls",
     "zls",
     "ts_ls",
+    "gopls",
 })
 
 vim.diagnostic.config({
